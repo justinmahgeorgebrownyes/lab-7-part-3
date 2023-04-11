ffdba6b930deaadc696921e416203adfb6aac774
masterbillwoo <91901581+justinmahgeorgebrownyes@users.noreply.github.com>
2023-04-11 17:50:25 -0400

    Add project files.

---
+++ src/MoveToRangeAction.cpp
@@ -0,0 +1,20 @@
+#include "MoveToRangeAction.h"
+
+#include <iostream>
+
+MoveToRangeAction::MoveToRangeAction()
+{
+	m_name = "Move To Range Action";
+}
+
+MoveToRangeAction::~MoveToRangeAction()
+= default;
+
+void MoveToRangeAction::Action()
+{
+	if(GetAgent()->GetActionState() != ActionState::MOVE_TO_RANGE)
+	{
+		std::cout << "Performing " << m_name << std::endl;
+		GetAgent()->SetActionState(ActionState::MOVE_TO_RANGE);
+	}
+}
